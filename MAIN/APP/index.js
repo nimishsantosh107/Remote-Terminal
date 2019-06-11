0cd51e8d0688afdf3e308fc68759ef6d9ec3314c
Nimish Santosh <nimishsantosh107@icloud.com>
2019-06-11 22:51:54 +0530

    curl done

--- MAIN/APP/index.js
+++ MAIN/APP/index.js
@@ -2,3 +2,20 @@ const express = require('express');
 const http = require('http');
 const path = require('path');
 const bodyParser = require('body-parser');
+
+var app = express();
+var server = http.Server(app);
+app.use(bodyParser.json());
+
+
+app.use('/',express.static(path.join(__dirname,'/public')));
+
+app.post('/link',(req,res)=>{
+	var link = req.body.link;
+	link = link.match(/(https).*/gi)[0];
+	console.log(link);
+	res.status(200).send('HI');
+});
+
+
+server.listen(3000,()=>{console.log('SERVER IS UP ON PORT 3000');});
--- MAIN/CLIENT SCRIPTS/CONNECT/connect/connect-data.sh
+++ MAIN/CLIENT SCRIPTS/CONNECT/connect/connect-data.sh
@@ -1,4 +1,26 @@
 #!/bin/bash
 
 sleep 2
-tmate show-messages > ~/.CONNECT/tmate/tmate_data
+#create tmate_data
+tmate show-messages | grep "web sesion" | tail -n1 > ~/.CONNECT/tmate/tmate_data
+
+#write to link.json
+LINK=$(cat ~/.CONNECT/tmate/tmate_data)
+cat >~/.CONNECT/tmate/link.json <<EOF
+    {
+      "link": "$LINK"
+    }
+EOF
+
+#clear tmate_data
+echo ""> ~/.CONNECT/tmate/tmate_data
+
+#send POST
+#LINK SUBJECT TO CHANGE
+curl -X POST -H "Content-Type: application/json" -d @/Users/client/.CONNECT/tmate/link.json http://localhost:3000/link
+
+
+
+
+
+
